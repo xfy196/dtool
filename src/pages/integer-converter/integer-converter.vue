f1db598c890b04d1f2f31646a2fb6772cac6150d
许富荧 <xfy196@outlook.com>
2024-09-02 17:45:37 +0800

    新加整数转换器

--- src/pages/integer-converter/integer-converter.vue
+++ src/pages/integer-converter/integer-converter.vue
@@ -2,7 +2,6 @@
 import { computed, ref } from "vue";
 import { convertBase } from "./integer-converter.service";
 import InputCopy from "@/components/InputCopy.vue";
-import { ErrorCircle16Regular } from "@vicons/fluent";
 const inputValue = ref("66");
 const baseValue = ref(10);
 const customeValue = ref(20);
