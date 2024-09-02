9923d69ef43f024d6b9fd131e4be6ac0887c4a0a
xufuying <xufuying@100doc.com.cn>
2024-09-02 22:47:49 +0800

    update

--- src/pages/base64-string-converter/base64-string-converter.vue
+++ src/pages/base64-string-converter/base64-string-converter.vue
@@ -4,18 +4,41 @@ import { useStorage } from "@vueuse/core";
 import { inject, ref } from "vue";
 import { textToBase64, base64ToText } from "@/utils/base64";
 import { computedRefreshable } from "@/composable/computedRefreshable";
+import { FormItemRule } from "naive-ui";
 const _function = inject("_function", "base64-string-converter");
 
 const encodeSafe = useStorage(`${_function}:encodeSafe`, false);
 const decodeSafe = useStorage(`${_function}:decodeSafe`, false);
 const encodeValue = ref("");
-const encodeBase64Value = ref("");
+const form = ref({
+  encodeBase64Value: "",
+});
+const rules = {
+  encodeBase64Value: {
+    validator(_rule: FormItemRule, value: string) {
+      try {
+        return base64ToText(value, {
+          urlSafe: decodeSafe.value,
+        });
+      } catch (error: any) {
+        return error;
+      }
+    },
+    trigger: ["input", "blur"],
+  },
+};
 const [base64Value] = computedRefreshable(() =>
   textToBase64(encodeValue.value, { urlSafe: encodeSafe.value })
 );
-const [decodeValue] = computedRefreshable(() =>
-  base64ToText(encodeBase64Value.value, { urlSafe: decodeSafe.value })
-);
+const [decodeValue] = computedRefreshable(() => {
+  try {
+    return base64ToText(form.value.encodeBase64Value, {
+      urlSafe: decodeSafe.value,
+    });
+  } catch (error) {
+    return "";
+  }
+});
 const { isSupported, copy: copyBase64 } = useCopy({
   source: base64Value,
   text: "Base64 复制成功",
@@ -29,7 +52,6 @@ const { copy: copyDecodeString } = useCopy({
 <template>
   <n-card title="字符串转base64">
     <n-form-item
-      :show-feedback="false"
       label-placement="left"
       label-align="left"
       :label="'安全URL编码'"
@@ -37,9 +59,7 @@ const { copy: copyDecodeString } = useCopy({
       <n-switch v-model:value="encodeSafe" />
     </n-form-item>
     <n-form-item
-      :show-feedback="false"
       label-align="left"
-      class="mt-4"
       :label="'字符串编码'"
     >
       <n-input
@@ -49,9 +69,7 @@ const { copy: copyDecodeString } = useCopy({
       ></n-input>
     </n-form-item>
     <n-form-item
-      :show-feedback="false"
       label-align="left"
-      class="mt-4"
       :label="'Base64编码结果'"
     >
       <n-input
@@ -61,47 +79,46 @@ const { copy: copyDecodeString } = useCopy({
         :value="base64Value"
       ></n-input>
     </n-form-item>
-    <div class="flex items-center justify-center mt-4">
+    <div class="flex items-center justify-center">
       <n-button tertiary @click.stop="copyBase64" v-if="isSupported"
         >Copy base64</n-button
       >
     </div>
   </n-card>
   <n-card title="Base64转字符串">
-    <n-form-item
-      :show-feedback="false"
-      label-placement="left"
-      label-align="left"
-      :label="'安全URL解码'"
-    >
-      <n-switch v-model:value="decodeSafe" />
-    </n-form-item>
-    <n-form-item
-      :show-feedback="false"
-      label-align="left"
-      class="mt-4"
-      :label="'Base64解码'"
-    >
-      <n-input
-        type="textarea"
-        placeholder="请输入内容"
-        v-model:value="encodeBase64Value"
-      ></n-input>
-    </n-form-item>
-    <n-form-item
-      :show-feedback="false"
-      label-align="left"
-      class="mt-4"
-      :label="'字符串结果'"
-    >
-      <n-input
-        readonly
-        type="textarea"
-        placeholder="字符串结果"
-        :value="decodeValue"
-      ></n-input>
-    </n-form-item>
-    <div class="flex items-center justify-center mt-4">
+    <n-form :model="form" :rules="rules">
+      <n-form-item
+        label-placement="left"
+        label-align="left"
+        :label="'安全URL解码'"
+      >
+        <n-switch v-model:value="decodeSafe" />
+      </n-form-item>
+      <n-form-item
+        label-align="left"
+        path="encodeBase64Value"
+        :label="'Base64解码'"
+      >
+        <n-input
+          type="textarea"
+          placeholder="请输入内容"
+          v-model:value="form.encodeBase64Value"
+        ></n-input>
+      </n-form-item>
+      <n-form-item
+        label-align="left"
+        :label="'字符串结果'"
+      >
+        <n-input
+          readonly
+          type="textarea"
+          placeholder="字符串结果"
+          :value="decodeValue"
+        ></n-input>
+      </n-form-item>
+    </n-form>
+
+    <div class="flex items-center justify-center">
       <n-button tertiary @click.stop="copyDecodeString" v-if="isSupported"
         >Copy decode string</n-button
       >
--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,23 +1,71 @@
-import { encode, decode, isValid } from "js-base64";
+import {
+  encode,
+  decode,
+  isValid,
+  toUint8Array,
+  fromUint8Array,
+} from "js-base64";
 
 function textToBase64(
   text: string,
   { urlSafe = false }: { urlSafe?: boolean }
 ) {
-  console.log("🚀 ~ urlSafe:", urlSafe)
-  return encode(text);
+  const encodeText = encode(text);
+  return urlSafe ? makeUrlSafe(encodeText) : encodeText;
 }
 function base64ToText(
   text: string,
   { urlSafe = false }: { urlSafe?: boolean }
 ) {
-  console.log("🚀 ~ urlSafe:", urlSafe)
-  return decode(text);
+  if (!isBase64(text, { urlSafe })) {
+    throw new Error("Invalid base64 string");
+  }
+  let cleanStr = removePotentialDataAndMimePrefix(text);
+  if (urlSafe) {
+    cleanStr = unURL(cleanStr);
+  }
+  try {
+    return decode(cleanStr);
+  } catch (error) {
+    throw new Error("Invalid base64 string");
+  }
 }
-function isBase64(text: string) {
+function isBase64(text: string, { urlSafe = false }: { urlSafe?: boolean }) {
+  // 清除data，base64前缀
+  let cleanStr = removePotentialDataAndMimePrefix(text);
+  if (urlSafe) {
+    cleanStr = unURL(cleanStr);
+  }
+  try {
+    const reEncodeBase64 = fromUint8Array(toUint8Array(cleanStr));
+    if (urlSafe) {
+      return removePotentialPadding(reEncodeBase64) === cleanStr;
+    }
+    return reEncodeBase64 === cleanStr.replace(/\s/g, "");
+  } catch (error) {
+    return false;
+  }
   return isValid(text);
 }
-function removePotentialDataAndMimePrefix(text: string){
-    return text.replace(/^data:.*?;base64,/, "");
+function removePotentialDataAndMimePrefix(text: string) {
+  return text.replace(/^data:.*?;base64,/, "");
 }
-export { textToBase64, isBase64, base64ToText, removePotentialDataAndMimePrefix };
+function makeUrlSafe(text: string) {
+  return text.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+}
+function removePotentialPadding(text: string) {
+  return text.replace(/=/g, "");
+}
+function unURL(text: string): string {
+  return text
+    .replace(/-/g, "+")
+    .replace(/_/g, "/")
+    .replace(/[^A-Za-z0-9+/]/g, "");
+}
+export {
+  textToBase64,
+  isBase64,
+  base64ToText,
+  removePotentialDataAndMimePrefix,
+  removePotentialPadding,
+};
