d2cdb34c2dd0ebe485451e96db5a929ed4f28c7d
许富荧 <xfy196@outlook.com>
2024-09-03 18:07:28 +0800

    更新罗马数字转换器和密码强弱检测器

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+/** @format */
+module.exports = {
+  printWidth: 80, //单行长度
+  tabWidth: 2, //缩进长度
+  semi: true, //句末使用分号
+  singleQuote: true, //使用单引号
+  bracketSpacing: true, //在对象前后添加空格-eg: { foo: bar }
+  arrowParens: 'always', //单参数箭头函数参数周围使用圆括号-eg: (x) => x
+  vueIndentScriptAndStyle: true, //对vue中的script及style标签缩进
+  trailingComma: 'none' //禁止随时添加逗号
+};
