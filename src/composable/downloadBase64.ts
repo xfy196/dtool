27b951573cb097e805fb0765e6b796fef27d9524
xufuying <xufuying@100doc.com.cn>
2024-09-06 00:28:29 +0800

    feat: base64 file converter get ✅

--- src/composable/downloadBase64.ts
+++ src/composable/downloadBase64.ts
@@ -1,38 +1,111 @@
-import { extension } from 'mime-types';
+import mime from 'mime';
+import { Ref } from 'vue';
 
-const getBase64MimeType = (base64: string) => {
-  const [, mimeType = undefined] = base64.match(/data:(.*?);base64/i) ?? [];
+const getBase64MimeType = (base64: string): string => {
+  const [, mimeType = ''] = base64.match(/data:(.*?);base64/i) ?? [];
   return mimeType;
 };
 const getExtensionFromMimeType = (
   mimeType: string | undefined,
   defaultExtension = 'txt'
-) => {
+): string => {
   if (mimeType) {
-    return extension(mimeType) ?? defaultExtension;
+    const ext = mime.getExtension(mimeType);
+    return ext === null ? defaultExtension : ext;
   }
   return defaultExtension;
 };
 const downloadBase64File = ({
-  sourceValue,
+  source,
   fileName,
   extension,
   fileMimeType
 }: {
-  sourceValue: string;
+  source: string;
   fileName?: string;
   extension?: string;
   fileMimeType?: string;
 }) => {
-  console.log('🚀 ~ fileMimeType:', fileMimeType);
-  console.log('🚀 ~ extension:', extension);
-  if (!sourceValue) {
+  if (!source) {
     throw new Error('base64 is empty');
   }
-  let base64String = '';
+  let base64String = source;
+  const mimeType = getBase64MimeType(source);
+  if (!mimeType) {
+    const targetMimeType = fileMimeType ?? mime.getType(extension ?? 'txt');
+    base64String = `data:${targetMimeType};base64,${source}`;
+  }
+  const ext = extension || getExtensionFromMimeType(mimeType);
+  let cleanFileName = fileName ?? `${fileName}.${ext}`;
+  if (ext && !cleanFileName.endsWith(`.${ext}`)) {
+    cleanFileName = `${cleanFileName}.${ext}`;
+  }
   const a = document.createElement('a');
   a.href = base64String;
-  a.download = fileName || '';
+  a.download = cleanFileName || '';
   a.click();
 };
-export { getBase64MimeType, getExtensionFromMimeType, downloadBase64File };
+
+const useDownloadBase64File = ({
+  source,
+  fileName,
+  extension
+}: {
+  source: Ref<string>;
+  fileName?: string;
+  extension?: string;
+}) => {
+  return {
+    download() {
+      return downloadBase64File({
+        source: source.value,
+        fileName,
+        extension
+      });
+    }
+  };
+};
+const useDownloadBase64FileRef = ({
+  source,
+  fileName,
+  extension
+}: {
+  source: Ref<string>;
+  fileName?: Ref<string>;
+  extension?: Ref<string>;
+}) => {
+  return {
+    download() {
+      return downloadBase64File({
+        source: source.value,
+        fileName: fileName?.value,
+        extension: extension?.value
+      });
+    }
+  };
+};
+const usePreviewImgFormBase64 = (base64String: string) => {
+  if (!base64String) {
+    throw new Error('base64 is empty');
+  }
+  const img = document.createElement('img');
+  img.src = base64String;
+  const container = document.createElement('div');
+  container.appendChild(img);
+  const previewContainer = document.getElementById('preview-container');
+  if (previewContainer) {
+    previewContainer.innerHTML = '';
+    previewContainer.appendChild(container);
+  } else {
+    throw new Error('preview-container is not found');
+  }
+  return img;
+};
+export {
+  getBase64MimeType,
+  getExtensionFromMimeType,
+  downloadBase64File,
+  useDownloadBase64File,
+  useDownloadBase64FileRef,
+  usePreviewImgFormBase64
+};
--- src/pages/base64-file-converter/base64-file-converter.vue
+++ src/pages/base64-file-converter/base64-file-converter.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
   import { useStyleStore } from '@/stores/style.store';
   import { storeToRefs } from 'pinia';
-  import { ref, watch } from 'vue';
+  import { Ref, ref, watch } from 'vue';
   import { isBase64 } from '@/utils/base64';
-  import { FormRules } from 'naive-ui';
+  import { FormRules, UploadFileInfo, useMessage } from 'naive-ui';
   import {
     getBase64MimeType,
-    getExtensionFromMimeType
+    getExtensionFromMimeType,
+    useDownloadBase64FileRef,
+    usePreviewImgFormBase64
   } from '@/composable/downloadBase64';
+  import { useBase64 } from '@vueuse/core';
+  import { useCopy } from '@/composable/copy';
+  const fileBase64 = ref('');
+  const filename = ref('file');
+  const extension = ref('');
   const base64Form = ref({
-    filename: 'file',
-    extension: '',
-    fileBase64: ''
+    fileBase64
   });
-
   const base64FormRules: FormRules = {
     fileBase64: [
       {
@@ -33,10 +37,63 @@
   watch(
     () => base64Form.value.fileBase64,
     (val) => {
-      console.log('🚀 ~ watch ~ val:', val);
-      console.log(getExtensionFromMimeType(getBase64MimeType(val)));
+      const mimeType = getBase64MimeType(val);
+      if (mimeType) {
+        extension.value = getExtensionFromMimeType(mimeType);
+      }
     }
   );
+
+  const { download } = useDownloadBase64FileRef({
+    source: fileBase64,
+    fileName: filename,
+    extension: extension
+  });
+  // 下载
+  const handleDownload = () => {
+    try {
+      if (!isBase64(fileBase64.value, { urlSafe: true })) {
+        return;
+      }
+      download();
+    } catch (error) {
+      console.log('🚀 ~ handleDownload ~ error:', error);
+    }
+  };
+  // 预览
+  const handlePreview = () => {
+    try {
+      if (!isBase64(fileBase64.value, { urlSafe: true })) {
+        return;
+      }
+      const img = usePreviewImgFormBase64(fileBase64.value);
+      img.style.maxWidth = '100%';
+      img.style.maxHeight = '400px';
+      const previewContainer = document.getElementById('preview-container');
+      if (previewContainer) {
+        previewContainer.innerHTML = '';
+        previewContainer.appendChild(img);
+      }
+    } catch (error) {
+      console.log('🚀 ~ handlePreview ~ error:', error);
+    }
+  };
+  const fileInput = ref() as Ref<File>;
+  const { base64: base64Res } = useBase64(fileInput);
+  const { copy } = useCopy({
+    source: base64Res,
+    text: 'Base64 string copied to clipboard!'
+  });
+  const message = useMessage();
+  const handleChangeFile = ({ file }: { file: UploadFileInfo }) => {
+    if (file.file) {
+      if (file.file.size <= 1024 * 1024 * 20) {
+        fileInput.value = file.file;
+        return;
+      }
+      message.error("File size can't be larger than 20MB");
+    }
+  };
 </script>
 
 <template>
@@ -45,18 +102,19 @@
       <n-grid :x-gap="10">
         <n-form-item-gi label="File Name" :span="isSmallScreen ? 14 : 16">
           <n-input
-            v-model:value="base64Form.filename"
+            v-model:value="filename"
             placeholder="Download FileName"
           ></n-input>
         </n-form-item-gi>
         <n-form-item-gi label="Extension" :span="isSmallScreen ? 10 : 8">
-          <n-input
-            v-model="base64Form.extension"
-            placeholder="Extension"
-          ></n-input>
+          <n-input v-model:value="extension" placeholder="Extension"></n-input>
         </n-form-item-gi>
       </n-grid>
-      <n-form-item path="fileBase64" label-placement="left">
+      <n-form-item
+        :show-feedback="false"
+        path="fileBase64"
+        label-placement="left"
+      >
         <n-input
           :rows="4"
           type="textarea"
@@ -64,15 +122,50 @@
           placeholder="请输入base64字符串"
         ></n-input>
       </n-form-item>
-      <div class="flex justify-center items-center">
+      <div class="flex justify-center py-2">
+        <div id="preview-container" />
+      </div>
+      <div class="flex items-center justify-center">
         <n-space>
-          <n-button tertiary>Preview Image</n-button>
-          <n-button tertiary>Download File</n-button>
+          <n-button tertiary @click.stop="handlePreview"
+            >Preview Image</n-button
+          >
+          <n-button @click.stop="handleDownload" tertiary
+            >Download File</n-button
+          >
         </n-space>
       </div>
     </n-form>
   </n-card>
-  <n-card title="File to Base64"></n-card>
+  <n-card title="File to Base64">
+    <n-form-item label-placement="left">
+      <n-upload
+        :show-file-list="false"
+        @change="handleChangeFile"
+        directory-dnd
+      >
+        <n-upload-dragger>
+          <n-text class="mb-3">
+            Drag and drop a file here, or click to select a file
+          </n-text>
+          <n-divider> or </n-divider>
+          <n-button>Select File</n-button>
+        </n-upload-dragger>
+      </n-upload>
+    </n-form-item>
+    <n-form-item label-placement="left">
+      <n-input
+        placeholder="文件的Base64内容"
+        :rows="4"
+        type="textarea"
+        readonly
+        :value="base64Res"
+      ></n-input>
+    </n-form-item>
+    <div class="flex items-center justify-center">
+      <n-button @click.stop="copy" tertiary>Copy</n-button>
+    </div>
+  </n-card>
 </template>
 
 <style lang="scss" scoped></style>
