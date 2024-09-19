9108f61f773c0cb91a30ba1de0a2c02c146ce310
xufuying <xufuying@100doc.com.cn>
2024-09-20 00:03:38 +0800

    fix: icon not show ❎

--- src/pages/toml-to-json/index.ts
+++ src/pages/toml-to-json/index.ts
@@ -1,11 +1,11 @@
-import { Braces } from '@vicons/tabler';
+import { DataArrayFilled } from '@vicons/material';
 import { defineTool } from '../tool';
 import { translate } from '../../plugins/i18n.plugins';
 export const tool = defineTool({
   name: translate('tools.toml-to-json.title'),
   path: '/toml-to-json',
   description: translate('tools.toml-to-json.description'),
-  icon: Braces,
+  icon: DataArrayFilled,
   keywords: ['toml', 'json', 'converter', 'toml to json'],
   show: true,
   component: () => import('./toml-to-json.vue'),
--- src/pages/toml-to-xml/index.ts
+++ src/pages/toml-to-xml/index.ts
@@ -1,11 +1,11 @@
-import { Braces } from '@vicons/tabler';
+import { DataArrayFilled } from '@vicons/material';
 import { defineTool } from '../tool';
 import { translate } from '../../plugins/i18n.plugins';
 export const tool = defineTool({
   name: translate('tools.toml-to-xml.title'),
   path: '/toml-to-xml',
   description: translate('tools.toml-to-xml.description'),
-  icon: Braces,
+  icon: DataArrayFilled,
   keywords: ['toml', 'json', 'converter', 'toml to xml', 'xml'],
   show: true,
   component: () => import('./toml-to-xml.vue'),
